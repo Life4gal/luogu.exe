90a33b65134c5f18a8245b8a44bac9ae817ad801
life4gal <life4gal@gmail.com>
2021-02-22 16:27:12 +0800

    P1004

---
+++ include/P1004.hpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+template<typename T, typename U, typename... More>
+auto max(T&& t, U&& u, More&&... more) {
+	if constexpr (sizeof...(more) == 0) {
+		return t > u ? t : u;
+	} else {
+		return max(max(t, u), more...);
+	}
+}
+
+int P1004() {
+	int field[12][12]{};
+	int step[12][12][12][12]{};
+
+	int n;
+	std::cin >> n;
+	while (true) {
+		int x;
+		int y;
+		int value;
+		std::cin >> x >> y >> value;
+		if (x == 0 && y == 0) {
+			break;
+		}
+		field[x][y] = value;
+	}
+	for (auto p1_x = 1; p1_x <= n; ++p1_x) {
+		for (auto p1_y = 1; p1_y <= n; ++p1_y) {
+			for (auto p2_x = 1; p2_x <= n; ++p2_x) {
+				for (auto p2_y = 1; p2_y <= n; ++p2_y) {
+					step[p1_x][p1_y][p2_x][p2_y] = max(
+							step[p1_x - 1][p1_y][p2_x - 1][p2_y],
+							step[p1_x - 1][p1_y][p2_x][p2_y - 1],
+							step[p1_x][p1_y - 1][p2_x - 1][p2_y],
+							step[p1_x][p1_y - 1][p2_x][p2_y - 1]);
+
+					auto value = field[p1_x][p1_y] + field[p2_x][p2_y];
+					if (p1_x == p2_x && p1_y == p2_y) {
+						step[p1_x][p1_y][p2_x][p2_y] += value / 2;
+					} else {
+						step[p1_x][p1_y][p2_x][p2_y] += value;
+					}
+				}
+			}
+		}
+	}
+	std::cout << step[n][n][n][n];
+
+	return 0;
+}
--- test/include.hpp
+++ test/include.hpp
@@ -2,3 +2,7 @@
 #include <P1001.hpp>
 #include <P1002.hpp>
 #include <P1003.hpp>
+#include <P1004.hpp>
+
+
+
