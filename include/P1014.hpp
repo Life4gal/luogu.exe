ee4c4dcd50510b4bf7ec0f3348648ded4a97515f
life4gal <life4gal@gmail.com>
2021-02-26 13:51:28 +0800

    P1014

--- include/P1014.hpp
+++ include/P1014.hpp
@@ -3,30 +3,9 @@
 
 int P1014()
 {
-	/*
-	 * line 1: 1 data
-	 * line 2: 2 data
-	 * line 3: 3 data
-	 * line 4: 4 data
-	 * ...
-	 * line n: n data
-	 *
-	 * let L(line) = line 1's data + line 2's data + ... + line line's data = line!
-	 * N --> line(line) <= N <= line(line+1) --> remainder = N - line(line)
-	 *
-	 * Stirling's formula:
-	 *  n！~= (2*pi*n)^(1/2) * (n/e)^n
-	 *  ln(n!) ~= ln((2*pi*n)^(1/2) * (n/e)^n) = ln((2*pi*n)^(1/2)) + ln((n/e)^n)
-	 *  ln(n!) ~= 1/2 * ln(2*pi*n) + n * ln(n/e)
-	 */
 
 	int n;
 	std::cin >> n;
-
-	// no!!! we have (n!), and we need to know n, not how many digit (n!) has!!!
-//	constexpr auto PI = 3.1415926;
-//	constexpr auto E = 2.7182818;
-//	std::cout << std::ceil(std::log10(2 * PI * n) + n*std::log10(1 / E * n)) << std::endl;
 
 	/*
 	 * 1/1 1/2 1/3 1/4 1/5 1/6
