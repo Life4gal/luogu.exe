00236be1a2eef8bfcc74d2f0b3acfad56a135d33
life4gal <life4gal@gmail.com>
2021-02-26 14:00:12 +0800

    P1009

--- include/P1009.hpp
+++ include/P1009.hpp
@@ -4,6 +4,27 @@
 #include <vector>
 
 int P1009() {
+	/*
+	 * line 1: 1 data
+	 * line 2: 2 data
+	 * line 3: 3 data
+	 * line 4: 4 data
+	 * ...
+	 * line n: n data
+	 *
+	 * let L(line) = line 1's data + line 2's data + ... + line line's data = line!
+	 * N --> line(line) <= N <= line(line+1) --> remainder = N - line(line)
+	 *
+	 * Stirling's formula:
+	 *  n！~= (2*pi*n)^(1/2) * (n/e)^n
+	 *  ln(n!) ~= ln((2*pi*n)^(1/2) * (n/e)^n) = ln((2*pi*n)^(1/2)) + ln((n/e)^n)
+	 *  ln(n!) ~= 1/2 * ln(2*pi*n) + n * ln(n/e)
+	 *
+	 *  constexpr auto PI = 3.1415926;
+	 *  constexpr auto E = 2.7182818;
+	 *  std::ceil(std::log10(2 * PI * n) + n * std::log10(1 / E * n));
+	 */
+
 	int n;
 	std::cin >> n;
 
